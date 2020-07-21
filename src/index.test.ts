db9bc417b6b5b6a2793bed02458e7dc3db458375
Jose Castillo Areiza <josecastilloareiza@Joses-MacBook-Pro.local>
2020-07-21 14:48:25 -0500

    Redefine concept

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,13 +2,6 @@ import { FitnessWatch } from './index';
 import { VehicleType } from './vehicleType';
 
 test('Can calculate average by age', () => {
-	const fitnessWatchtest = new FitnessWatch(242342)
- 
-    expect(() => 
-{
-	return fitnessWatchtest.getAverage(13124241)
-}
-
-    	 ).toReturn();
+	const fitnessWatchtest = new FitnessWatch(242342);
  
 });
